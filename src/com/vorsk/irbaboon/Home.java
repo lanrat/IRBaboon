7ba8eca1ac079910aad5acc50f6f3aba5fe453aa
Lester Villaspir <lester.villaspir@gmail.com>
2012-11-29 23:38:09 -0800

    changed ui

--- src/com/vorsk/irbaboon/Home.java
+++ src/com/vorsk/irbaboon/Home.java
@@ -5,6 +5,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.hardware.usb.UsbManager;
@@ -23,32 +24,20 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 //import com.hoho.android.usbserial.R;
 import com.hoho.android.usbserial.driver.UsbSerialDriver;
 import com.hoho.android.usbserial.driver.UsbSerialProber;
-import com.hoho.android.usbserial.util.HexDump;
 import com.hoho.android.usbserial.util.SerialInputOutputManager;
 
 
-public class Home extends FragmentActivity implements ActionBar.TabListener {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide fragments for each of the
-     * sections. We use a {@link android.support.v4.app.FragmentPagerAdapter} derivative, which will
-     * keep every loaded fragment in memory. If this becomes too memory intensive, it may be best
-     * to switch to a {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    ViewPager mViewPager;
+public class Home extends Activity {
     
     SerialInputOutputManager mSerialIoManager;
     UsbManager mUsbManager;
     UsbSerialDriver mSerialDevice;
+    TextView globalTextView;
     
     private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
     
@@ -80,55 +69,19 @@ public class Home extends FragmentActivity implements ActionBar.TabListener {
         
         // Create USB Serial Manager
         mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
-        
-        // Create the adapter that will return a fragment for each of the three primary sections
-        // of the app.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the action bar.
-        final ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        // When swiping between different sections, select the corresponding tab.
-        // We can also use ActionBar.Tab#select() to do this if we have a reference to the
-        // Tab.
-        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                actionBar.setSelectedNavigationItem(position);
-            }
-        });
-
-        // For each of the sections in the app, add a tab to the action bar.
-        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-            // Create a tab with text corresponding to the page title defined by the adapter.
-            // Also specify this Activity object, which implements the TabListener interface, as the
-            // listener for when this tab is selected.
-            actionBar.addTab(
-                    actionBar.newTab()
-                            .setText(mSectionsPagerAdapter.getPageTitle(i))
-                            .setTabListener(this));
-        }
-    }
+     }
     
     public void onResume(){
     	super.onResume();
-    	
     	// connecting to usb device
         mSerialDevice = UsbSerialProber.acquire(mUsbManager);
-        Log.d(TAG, "Resumed, mSerialDevice=" + mSerialDevice);
         if (mSerialDevice == null) {
-            //mTitleTextView.setText("No serial device.");
         } else {
             try {
                 mSerialDevice.open();
             } catch (IOException e) {
                 Log.e(TAG, "Error setting up device: " + e.getMessage(), e);
-                //mTitleTextView.setText("Error opening device: " + e.getMessage());
                 try {
                     mSerialDevice.close();
                 } catch (IOException e2) {
@@ -137,7 +90,6 @@ public class Home extends FragmentActivity implements ActionBar.TabListener {
                 mSerialDevice = null;
                 return;
             }
-            //mTitleTextView.setText("Serial device: " + mSerialDevice);
         }
         onDeviceStateChange();
     	
@@ -153,7 +105,6 @@ public class Home extends FragmentActivity implements ActionBar.TabListener {
 
     private void startIoManager() {
         if (mSerialDevice != null) {
-            Log.i(TAG, "Starting io manager ..");
             mSerialIoManager = new SerialInputOutputManager(mSerialDevice, mListener);
             mExecutor.submit(mSerialIoManager);
         }
@@ -167,16 +118,15 @@ public class Home extends FragmentActivity implements ActionBar.TabListener {
     private void updateReceivedData(byte[] data) {
     	
     	String message = "Read " + data.length + " bytes: \n";
-    	
+    	    	
     	for (int i = 0; i < data.length; i++)
     	{
-    		message += Integer.toBinaryString((int) data[i]);
+    		message += Integer.toHexString((int) data[i]) + " ";
     	}
     	message +="\n";
     	
-    	TextView view = ((DummySectionFragment)mSectionsPagerAdapter.getItem(0)).textView;
-        view.append(message);
-
+    	Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    	globalTextView = (TextView)findViewById(R.id.textview);
     }
 
     @Override
@@ -186,78 +136,4 @@ public class Home extends FragmentActivity implements ActionBar.TabListener {
     }
 
     
-
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
-
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        // When the given tab is selected, switch to the corresponding page in the ViewPager.
-        mViewPager.setCurrentItem(tab.getPosition());
-    }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
-    
-    
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to one of the primary
-     * sections of the app.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int i) {
-            Fragment fragment = new DummySectionFragment();
-            Bundle args = new Bundle();
-            args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, i + 1);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public int getCount() {
-        	// number of items
-            return 3;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0: return "Home";//return getString(R.string.title_section1).toUpperCase();
-                case 1: return "Remote";//getString(R.string.title_section2).toUpperCase();
-                case 2: return "Some\nSection\nName";//getString(R.string.title_section3).toUpperCase();
-            }
-            return null;
-        }
-    }
-
-    /**
-     * A dummy fragment representing a section of the app, but that simply displays dummy text.
-     */
-    public static class DummySectionFragment extends Fragment {
-    	public TextView textView;
-    	
-        public DummySectionFragment() {
-        }
-
-        public static final String ARG_SECTION_NUMBER = "section_number";
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            textView = new TextView(getActivity());
-            //textView.setGravity(Gravity.CENTER);
-            Bundle args = getArguments();
-            textView.setText(Integer.toString(args.getInt(ARG_SECTION_NUMBER)) + "\n");
-            return textView;
-        }
-    }
 }
